67abc3cadd430488530e0262fa4c91379f8bace9
Tegar Ferdigantara <tegarferdigantara007@student.untan.ac.id>
2021-05-29 23:02:57 +0700

    Praktikum -12 Trait

--- implementasi_trait.php
+++ implementasi_trait.php
@@ -7,7 +7,5 @@ require_once('vendor/autoload.php');
 $tegar = new Person;
 
 $tegar->goodBye('Tegar Ferdigantara');
-// $tegar->hasName = "Tegar Ferdigantara";
-// echo $tegar->hasName;
 
 echo $tegar->birthDate('24', 'September', '2001');
